13c0dfcfddc476e4c0815884c1c6dd868538bd3f
Bishwa Mandal <mandalbishwarup@gmail.com>
2023-08-09 23:23:10 +0530

    Commit & Updated

--- app/src/main/java/com/example/tourismapp/Activities/MainActivity.java
+++ app/src/main/java/com/example/tourismapp/Activities/MainActivity.java
@@ -1,29 +1,59 @@
 package com.example.tourismapp.Activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
-import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.tourismapp.Adapters.PopularAdapter;
 import com.example.tourismapp.Domains.PopularDomain;
 import com.example.tourismapp.R;
+import com.example.tourismapp.databinding.ActivityMainBinding;
 import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
     private RecyclerView.Adapter adapterPopular;
     private RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(R.layout.activity_main);
 
         initRecyclerView();
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+            if (item.getItemId() == R.id.bottom_home) {
+
+
+            } else if (item.getItemId() == R.id.bottom_places) {
+
+
+            } else if (item.getItemId() == R.id.bottom_account) {
+
+
+            } else if (item.getItemId() == R.id.bottom_logout) {
+                new MaterialAlertDialogBuilder(this)
+                        .setTitle("Logout")
+                        .setMessage("Do you want to logout?")
+                        .setPositiveButton("Yes", (dialogInterface, i) -> finishActivity())
+                        .setNegativeButton("No", (dialogInterface, i) -> dialogInterface.dismiss())
+                        .show();
+            }
+            return true;
+        });
+    }
+
+    public void finishActivity() {
+        Intent logout = new Intent(this, LoginActivity.class);
+        startActivity(logout);
     }
 
     private void initRecyclerView() {
