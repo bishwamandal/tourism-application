72d04952d703072b767579bad10d1cab70267ba4
Bishwa Mandal <mandalbishwarup@gmail.com>
2023-08-13 18:56:09 +0530

    Commit & Updated

--- app/src/main/java/com/example/tourismapp/Activities/PlacesActivity.java
+++ app/src/main/java/com/example/tourismapp/Activities/PlacesActivity.java
@@ -1,132 +1,40 @@
 package com.example.tourismapp.Activities;
 
-import android.app.Dialog;
-import android.content.Intent;
-import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
-import android.os.Bundle;
-import android.view.Gravity;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.view.Window;
-import android.widget.LinearLayout;
-import android.widget.Toast;
-
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.SearchView;
-import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
 
 import com.example.tourismapp.Adapters.PopularAdapter;
 import com.example.tourismapp.Domains.PopularDomain;
 import com.example.tourismapp.R;
 import com.google.android.material.appbar.MaterialToolbar;
-import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {
+public class PlacesActivity extends AppCompatActivity {
 
-    MaterialToolbar topAppBar;
+    private MaterialToolbar back;
     private RecyclerView.Adapter adapterPopular;
     private RecyclerView recyclerView;
-
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_places);
 
-        topAppBar = findViewById(R.id.topAppBar);
-        topAppBar.setNavigationOnClickListener(new View.OnClickListener() {
+        back = findViewById(R.id.places_toolbar);
+        back.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                showDialog();
-            }
-        });
-
-        topAppBar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
-            @Override
-            public boolean onMenuItemClick(MenuItem item) {
-                if (item.getItemId() == R.id.toolbar_userprofile) {
-                    accountProfile();
-                    return true;
-                } else {
-                    return false;
-                }
+                finish();
             }
         });
 
         initRecyclerView();
-
-    }
-
-    private void accountProfile() {
-        Intent accountprofile = new Intent(MainActivity.this, ProfileActivity.class);
-        startActivity(accountprofile);
-    }
-
-    private void showDialog() {
-        final Dialog dialog = new Dialog(this);
-        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
-        dialog.setContentView(R.layout.bottomsheet_layout);
-
-        LinearLayout home = dialog.findViewById(R.id.LayoutHome);
-        LinearLayout places = dialog.findViewById(R.id.LayoutAllPlaces);
-        LinearLayout profile = dialog.findViewById(R.id.LayoutProfile);
-        LinearLayout logout = dialog.findViewById(R.id.LayoutLogout);
-
-        home.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "Home", Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        places.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "All Places", Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        profile.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "Profile", Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        logout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                userLogout();
-            }
-        });
-
-        dialog.show();
-        dialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
-        dialog.getWindow().getAttributes().windowAnimations = R.style.DialogAnimation;
-        dialog.getWindow().setGravity(Gravity.BOTTOM);
-    }
-
-    private void userLogout() {
-        new MaterialAlertDialogBuilder(this)
-                .setTitle("Logout")
-                .setMessage("Do you want to logout?")
-                .setPositiveButton("Yes", (dialogInterface, i) -> finishActivity())
-                .setNegativeButton("No", (dialogInterface, i) -> dialogInterface.dismiss())
-                .show();
-    }
-
-    private void finishActivity() {
-        Intent log_out = new Intent(MainActivity.this, LoginActivity.class);
-        startActivity(log_out);
     }
 
     private void initRecyclerView() {
@@ -181,11 +89,6 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public void onBackPressed() {
-        new MaterialAlertDialogBuilder(this)
-                .setTitle("Exit App")
-                .setMessage("Do you want to exit the app?")
-                .setPositiveButton("Yes", (dialogInterface, i) -> finishAffinity())
-                .setNegativeButton("No", (dialogInterface, i) -> dialogInterface.dismiss())
-                .show();
+        finish();
     }
 }
