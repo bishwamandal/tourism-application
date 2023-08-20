a8fe2dd8f90ea4ca74602698eecbbc3833bbf290
Bishwa Mandal <mandalbishwarup@gmail.com>
2023-08-20 21:08:43 +0530

    Commit & Updated

--- app/src/main/java/com/example/tourismapp/Activities/MainActivity.java
+++ app/src/main/java/com/example/tourismapp/Activities/MainActivity.java
@@ -163,7 +163,7 @@ public class MainActivity extends AppCompatActivity {
                 +"Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
                 +"Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."));
         items.add(new PopularDomain(
-                "Place Name","Location", "ic_carousel1", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+                "Place Name","Location", "img1", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
                 +"Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
                 +"Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
                 +"Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."));
